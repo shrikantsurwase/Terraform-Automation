bb6513f0e9e8ce9ec2883db2bbf5db26a8e38266
shrikantsurwase <106244677+shrikantsurwase@users.noreply.github.com>
2024-01-01 15:25:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform-batch-21"
+    bucket = "mydev-tf-project-terraform-batch-project-automation-21"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
