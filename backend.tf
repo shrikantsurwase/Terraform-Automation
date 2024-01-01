522e279b3d661c6c2faea8218338621bb469b8d8
shrikantsurwase <106244677+shrikantsurwase@users.noreply.github.com>
2024-01-01 15:34:22 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "mydev-tf-project-terraform-batch-project-automation-21"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "my-DybanoDB-Table-Project"
   }
 }
