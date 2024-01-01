fc538404c4f17f67da5ee48a0f58c64176d39e63
shrikantsurwase <106244677+shrikantsurwase@users.noreply.github.com>
2024-01-01 23:23:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "ap-south-1" 
+       default     = "us-east-1" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "batch-22" 
+    default     =  "Shrikant_key" 
 }
 
 variable "instance_type" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-07ffb2f4d65357b42" 
+    default     = "ami-079db87dc4c10ac91" 
 }
 variable "versioning" {
     type        = bool
